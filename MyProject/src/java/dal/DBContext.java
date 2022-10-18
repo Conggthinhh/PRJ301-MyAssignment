41f5a4f2e0f90e120fabc02ab267b29c4f7ae674
Conggthinhh <thinhkche161160@fpt.edu.vn>
2022-10-18 11:24:59 +0700

    searchcontroller

---
+++ MyProject/src/java/dal/DBContext.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dal;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Mr Thinh
+ */
+public class DBContext {
+    protected Connection connection;
+    public DBContext(){
+        try {
+            String username = "dung";
+            String password = "12345";
+            String url = "jdbc:sqlserver://localhost\\SQLEXPRESS:1433;databaseName=ASSIGNMENT";
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            connection = DriverManager.getConnection(url, username, password);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(DBContext.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(DBContext.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
---
+++ MyProject/src/java/model/Student.java
@@ -0,0 +1,69 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author Mr Thinh
+ */
+
+public class Student {
+    private String stid;
+    private String stmember;
+    private String stsurname;
+    private String stmiddlename;
+    private String stgivenname;
+
+    public Student() {
+    }
+
+    public Student(String stid, String stmember, String stsurname, String stmiddlename, String stgivenname) {
+        this.stid = stid;
+        this.stmember = stmember;
+        this.stsurname = stsurname;
+        this.stmiddlename = stmiddlename;
+        this.stgivenname = stgivenname;
+    }
+
+    public String getStid() {
+        return stid;
+    }
+
+    public void setStid(String stid) {
+        this.stid = stid;
+    }
+
+    public String getStmember() {
+        return stmember;
+    }
+
+    public void setStmember(String stmember) {
+        this.stmember = stmember;
+    }
+
+    public String getStsurname() {
+        return stsurname;
+    }
+
+    public void setStsurname(String stsurname) {
+        this.stsurname = stsurname;
+    }
+
+    public String getStmiddlename() {
+        return stmiddlename;
+    }
+
+    public void setStmiddlename(String stmiddlename) {
+        this.stmiddlename = stmiddlename;
+    }
+
+    public String getStgivenname() {
+        return stgivenname;
+    }
+
+    public void setStgivenname(String stgivenname) {
+        this.stgivenname = stgivenname;
+    }  
+}
